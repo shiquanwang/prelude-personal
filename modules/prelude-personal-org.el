cf82a268735365b56387a94da9da21b3990aed91
Shiquan Wang <shiquanwang@tencent.com>
2015-05-19 18:49:52 +0800

    Add: Add preliminary `prelude-personal-org.el`.

---
+++ modules/prelude-personal-org.el
@@ -0,0 +1,21 @@
+;;; prelude-personal-org.el --- org-mode configuration.
+;;
+;; Copyright (c) 2014 Shiquan Wang
+;;
+;; Author: Shiquan Wang <shiquanwang@gmail.com>
+;; URL: https://github.com/shiquanwang/prelude-personal
+;; Version: 0.0.1
+;; Keywords: convenience
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; Configurations for org-mode.
+
+;;; Code:
+
+(prelude-require-packages '(bbdb org org-plus-contrib smex))
+
+(provide 'prelude-personal-org)
+;;; prelude-personal-org.el ends here
