2f806f9951804733ef579c984a7dbc7427db14c8
Shiquan Wang <shiquanwang@tencent.com>
2015-05-19 11:56:18 +0800

    Add: Add `prelude-personal-packages.el`.

---
+++ core/prelude-personal-packages.el
@@ -0,0 +1,53 @@
+;;; prelude-personal-packages.el --- personal package selection.
+;;
+;; Copyright (c) 2013 Shiquan Wang
+;;
+;; Author: Shiquan Wang <shiquanwang@gmail.com>
+;; URL: https://github.com/shiquanwang/prelude-personal
+;; Version: 0.0.1
+;; Keywords: convenience
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; Takes care of the automatic installation of all the packages required by
+;; Emacs Prelude Personal.
+
+;;; Code:
+
+(require 'cl)
+(require 'package)
+
+(add-to-list 'package-archives
+             '("org" . "http://orgmode.org/elpa/") t)
+
+(package-initialize)
+
+(defvar prelude-personal-packages
+  '(
+    )
+  "A list of packages to ensure are installed at launch.")
+
+(defun prelude-personal-packages-installed-p ()
+  "Check if all packages in `prelude-personal-packages' are installed."
+  (every #'package-installed-p prelude-personal-packages))
+
+(defun prelude-personal-install-packages ()
+  "Install all packages listed in `prelude-personal-packages'."
+  (unless (prelude-personal-packages-installed-p)
+    ;; check for new packages (package versions)
+    (message "%s" "Emacs Prelude Personal is now refreshing its package database...")
+    (package-refresh-contents)
+    (message "%s" " done.")
+    ;; install the missing packages
+    (prelude-require-packages prelude-personal-packages)))
+
+(prelude-personal-install-packages)
+
+(provide 'prelude-personal-packages)
+;; Local Variables:
+;; byte-compile-warnings: (not cl-functions)
+;; End:
+
+;;; prelude-personal-packages.el ends here
